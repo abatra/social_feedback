4ebd7077f16faf5bcf5d161b4a1a9ce84c682ffe
Arjun Batra <arjunbatra@arjun-batras-computer.local>
2011-04-22 23:36:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialFeedback::Application.config.secret_token = 'f6f0fe8e275fa61145bd41259956fda0ebfe191d9e7149b3f2af19f7f23d1c3ad33dcc4f33a91b583166b37cd75c9fb43116fcd6d5d16b7665d999224021c634'
